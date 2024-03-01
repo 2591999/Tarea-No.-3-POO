b0cc2e3612582c703ccfcd4cb6c311c4946b3b98
2591999 <160382603+2591999@users.noreply.github.com>
2024-03-01 00:52:10 -0600

    Tarea No.3

---
+++ Seleccion 1.0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main (){
+	// i=+1
+	 /*int i = 0;
+ 	while(i<5){
+ 		cout<<"while: "<<i<<endl;
+		i++;	
+	}*/
+	
+	char respuesta='s';
+
+	while(respuesta=='s' || respuesta=='S'){
+		cout<<"Desea ingresar otro valor (S/N) ";
+		cin>>respuesta;
+		
+		
+ 		}
+	
+	system("pause");
+}
